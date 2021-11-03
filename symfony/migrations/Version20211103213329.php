274052c0dfc7c2bcbdf50c6cdf2b9e84676ebea9
root <root@DESKTOP-KGRKKO5.localdomain>
2021-11-03 20:26:05 +0100

    Test entity

---
+++ symfony/migrations/Version20211103213329.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211103213329 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE test_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE test (id INT NOT NULL, title VARCHAR(255) NOT NULL, description TEXT DEFAULT NULL, summary TEXT DEFAULT NULL, duration INT DEFAULT NULL, next_submission_after INT DEFAULT NULL, ttl INT DEFAULT NULL, use_pagination BOOLEAN DEFAULT NULL, questions_per_page INT DEFAULT NULL, shuffle_questions BOOLEAN DEFAULT NULL, immediate_answers BOOLEAN DEFAULT NULL, restrict_submissions BOOLEAN DEFAULT NULL, allowed_submissions INT DEFAULT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE test_id_seq CASCADE');
+        $this->addSql('DROP TABLE test');
+    }
+}
---
+++ symfony/src/Entity/Test.php
@@ -0,0 +1,324 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use App\Repository\TestRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * @ORM\Entity(repositoryClass=TestRepository::class)
+ * @ORM\Table(name="test")
+ * @ApiResource(
+ *   collectionOperations={
+ *     "get",
+ *     "post"={"security"="is_granted('ROLE_ADMIN')"}
+ *   },
+ *   itemOperations={
+ *     "get",
+ *     "put"={"security"="is_granted('ROLE_ADMIN')"},
+ *     "delete"={"security"="is_granted('ROLE_ADMIN')"},
+ *     "patch"={"security"="is_granted('ROLE_ADMIN')"}
+ *   }
+ * )
+ */
+class Test
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="validation.not_blank")
+     * @Assert\Type(
+     *   type="string",
+     *   message="validation.not_string"
+     * )
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     * @Assert\Type(
+     *   type="string",
+     *   message="validation.not_string"
+     * )
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     * @Assert\Type(
+     *   type="string",
+     *   message="validation.not_string"
+     * )
+     */
+    private $summary;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Type(
+     *   type="integer",
+     *   message="validation.not_int"
+     * )
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Type(
+     *   type="integer",
+     *   message="validation.not_int"
+     * )
+     */
+    private $nextSubmissionAfter;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Type(
+     *   type="integer",
+     *   message="validation.not_int"
+     * )
+     */
+    private $ttl;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     * @Assert\Type(
+     *   type="bool",
+     *   message="validation.not_bool"
+     * )
+     */
+    private $usePagination;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Type(
+     *   type="integer",
+     *   message="validation.not_int"
+     * )
+     */
+    private $questionsPerPage;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     * @Assert\Type(
+     *   type="bool",
+     *   message="validation.not_bool"
+     * )
+     */
+    private $shuffleQuestions;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     * @Assert\Type(
+     *   type="bool",
+     *   message="validation.not_bool"
+     * )
+     */
+    private $immediateAnswers;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     * @Assert\Type(
+     *   type="bool",
+     *   message="validation.not_bool"
+     * )
+     */
+    private $restrictSubmissions;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Type(
+     *   type="integer",
+     *   message="validation.not_int"
+     * )
+     */
+    private $allowedSubmissions;
+
+    // /**
+    //  * @ORM\OneToMany(targetEntity="App\Entity\Question", mappedBy="survey")
+    //  */
+    // private $questions;
+
+    // public function __construct()
+    // {
+    //     $this->questions = new ArrayCollection();
+    // }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSummary(): ?string
+    {
+        return $this->summary;
+    }
+
+    public function setSummary(?string $summary): self
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getNextSubmissionAfter(): ?int
+    {
+        return $this->nextSubmissionAfter;
+    }
+
+    public function setNextSubmissionAfter(?int $nextSubmissionAfter): self
+    {
+        $this->nextSubmissionAfter = $nextSubmissionAfter;
+
+        return $this;
+    }
+
+    public function getTtl(): ?int
+    {
+        return $this->ttl;
+    }
+
+    public function setTtl(?int $ttl): self
+    {
+        $this->ttl = $ttl;
+
+        return $this;
+    }
+
+    public function getUsePagination(): ?bool
+    {
+        return $this->usePagination;
+    }
+
+    public function setUsePagination(bool $usePagination): self
+    {
+        $this->usePagination = $usePagination;
+
+        return $this;
+    }
+
+    public function getQuestionsPerPage(): ?int
+    {
+        return $this->questionsPerPage;
+    }
+
+    public function setQuestionsPerPage(?int $questionsPerPage): self
+    {
+        $this->questionsPerPage = $questionsPerPage;
+
+        return $this;
+    }
+
+    public function getShuffleQuestions(): ?bool
+    {
+        return $this->shuffleQuestions;
+    }
+
+    public function setShuffleQuestions(?bool $shuffleQuestions): self
+    {
+        $this->shuffleQuestions = $shuffleQuestions;
+
+        return $this;
+    }
+
+    public function getImmediateAnswers(): ?bool
+    {
+        return $this->immediateAnswers;
+    }
+
+    public function setImmediateAnswers(?bool $immediateAnswers): self
+    {
+        $this->immediateAnswers = $immediateAnswers;
+
+        return $this;
+    }
+
+    public function getRestrictSubmissions(): ?bool
+    {
+        return $this->restrictSubmissions;
+    }
+
+    public function setRestrictSubmissions(?bool $restrictSubmissions): self
+    {
+        $this->restrictSubmissions = $restrictSubmissions;
+
+        return $this;
+    }
+
+    public function getAllowedSubmissions(): ?int
+    {
+        return $this->allowedSubmissions;
+    }
+
+    public function setAllowedSubmissions(?int $allowedSubmissions): self
+    {
+        $this->allowedSubmissions = $allowedSubmissions;
+
+        return $this;
+    }
+
+    // public function getQuestions(): Collection
+    // {
+    //     return $this->questions;
+    // }
+
+    // public function addQuestion(Question $question): self
+    // {
+    //     $this->questions->add($question);
+
+    //     return $this;
+    // }
+
+    // public function removeQuestion(Question $question): self
+    // {
+    //     $this->questions->removeElement($question);
+
+    //     return $this;
+    // }
+}
